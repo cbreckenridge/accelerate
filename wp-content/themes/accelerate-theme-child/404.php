6388ab9eebbaa8f037506c5a93438a2bbeb94193
Christine Breckenridge <cbreckenridge@ucmerced.edu>
2017-05-27 18:01:12 -0700

    added plugins

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,4 @@
+<?php
+get_header(); ?>
+<h1 id="four-o-four"><span>Sorry,</span> <br>&#8708; a page &isin; <?php echo $_SERVER['HTTP_HOST']; ?> &equiv; <?php echo $_SERVER['REQUEST_URI']; ?></h1>
+<?php get_footer(); ?>
