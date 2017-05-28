449eee77fa7bf3a717212f15f226d318c0c2bb5e
Christine Breckenridge <cbreckenridge@ucmerced.edu>
2017-05-27 20:51:59 -0700

    css fixes for about page

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -11,33 +11,31 @@
 get_header(); ?>
 
 	<div class="about-page">
-
 		<?php while ( have_posts() ) : the_post();?>
-
 			<div class="about-hero">
 				<h3><?php the_content(); ?></h3>
 			</div>
 			<h5>Our Services</h5>
 			<p id="our-services"><?php echo get_field('our_services');?></p>
 		<?php endwhile;?>
-		<?php query_posts('post_type=services&order=asc'); ?>
-		<?php while (have_posts()): the_post();
-			$icon = get_field('icon');
-			$size = 'full';
-		?>
-			<div id="services">
-				<div class="service">
-					<?php echo wp_get_attachment_image($icon,$size)?>
-					<div class="service-text">
-						<h2><?php the_title();?></h2>
-						<p><?php the_content();?></p>
+		<div id="services">
+			<?php query_posts('post_type=services&order=asc'); ?>
+			<?php while (have_posts()): the_post();
+				$icon = get_field('icon');
+				$size = 'full';
+			?>
+					<div class="service">
+						<?php echo wp_get_attachment_image($icon,$size)?>
+						<div class="service-text">
+							<h2><?php the_title();?></h2>
+							<p><?php the_content();?></p>
+						</div>
 					</div>
-				</div>
-			</div>
-		<?php endwhile; // end of the loop. ?>
+			<?php endwhile; // end of the loop. ?>
+		</div>
 		<div class="contact-button">
 			<h2>Interested in working with us?</h2>
-			<a class="button" href="/contact-us">Contact Us</a>
+			<a class="button" href="/accelerate/contact-us">Contact Us</a>
 		</div>
 	</div>
 
